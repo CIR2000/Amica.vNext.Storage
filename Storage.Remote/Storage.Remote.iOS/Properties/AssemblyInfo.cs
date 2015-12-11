aaa26bb6164b855eeccc18ede89d96b14e1b61ee
Nicola Iarocci <nicola@nicolaiarocci.com>
2015-12-11 11:53:26 +0100

    Major packages and code re-organization

--- Storage.Remote/Storage.Remote.iOS/Properties/AssemblyInfo.cs
+++ Storage.Remote/Storage.Remote.iOS/Properties/AssemblyInfo.cs
@@ -1,20 +1,26 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Storage.Core")]
+[assembly: AssemblyTitle("Storage.Remote.iOS")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CIR 2000")]
-[assembly: AssemblyProduct("Storage.Core")]
+[assembly: AssemblyProduct("Storage.Remote.iOS")]
 [assembly: AssemblyCopyright("Copyright © CIR 2000 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("014774a9-005d-4422-ba0b-a736d31c03ad")]
 
 // Version information for an assembly consists of the following four values:
 //
