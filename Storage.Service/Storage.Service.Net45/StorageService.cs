b085c81696193c64496b1b3e896475bd6a0d1539
Nicola Iarocci <nicola@nicolaiarocci.com>
2015-12-17 09:23:40 +0100

    Add the StorageService project

---
+++ Storage.Service/Storage.Service.Net45/StorageService.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Amica.vNext.Models;
+
+namespace Amica.vNext.Storage
+{
+    public class StorageService : ILocalBulkRepository, IRemoteRepository
+    {
+        public string ApplicationName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Get<T>(T obj) where T : BaseModel
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Insert<T>(T obj) where T : BaseModel
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete<T>(T obj) where T : BaseModel
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Replace<T>(T obj) where T : BaseModel
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<T>> Get<T>() where T : BaseModel
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<T>> Get<T>(string companyId) where T : BaseModelWithCompanyId
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<T>> Get<T>(DateTime? ifModifiedSince) where T : BaseModel
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<T>> Get<T>(DateTime? ifModifiedSince, string companyId) where T : BaseModelWithCompanyId
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IDictionary<string, T>> Get<T>(IEnumerable<string> uniqueIds) where T : BaseModel, new()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<T>> Insert<T>(IEnumerable<T> objs) where T : BaseModel
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<string>> Delete<T>(IEnumerable<T> objs) where T : BaseModel
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ClientId { get; set; }
+    }
+}
